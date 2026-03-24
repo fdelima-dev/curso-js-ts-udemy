faff24f06515f21dee7bbd5197d358d896bdb62c
fdelima-dev <fdelima.dev@gmail.com>
2026-03-24 18:03:38 -0300

    Aula06 Variavel let e declaração de variáveis

---
+++ aula06/index.js
@@ -0,0 +1,34 @@
+let nome = 'João'; //string
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log('Maria teve 1 filho com ',nome, ' em 2015');
+console.log('O filho de ',nome, " se chama Eduardo");
+
+let idade; //declarou variavel
+
+idade = 25; //inicializando variável
+
+console.log(idade);
+
+idade = 33;
+console.log(idade);
+
+//Não podemos criar variáveis com palavras reservada
+// 'let console'
+// 'let if'
+//Variáveis precisam ter nomes significativos
+//Não pode começar variável com número
+// let 1nome proibido
+// let nome1 aceitavel
+//Não pode conter espaços ou traços
+//utilizamos camelCase
+//Case Sensitive - letra maiúscula e letra minúscula faz diferença
+
+let nomeCliente = "Felipe";
+let nomecliente = "Lima";
+
+console.log(nomeCliente, nomecliente);
+
+//REGRA = não podemos recriar ou redeclarar a variavel com o comando let
+//NÃO UTILIZE VAR, UTILIZE LET
