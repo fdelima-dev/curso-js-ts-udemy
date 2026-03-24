64d52ac065ac75b72ba3907e97305c72ca68010f
fdelima-dev <fdelima.dev@gmail.com>
2026-03-24 12:37:38 -0300

    Adicionando aula04 comentarios.js

---
+++ aula04/comentarios.js
@@ -0,0 +1,15 @@
+// E escrever um comentário longo
+console.log('Olá mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+//comentário
+/*  
+um bloco de código
+Linha de comentário */
+/**
+ * Um título
+ * lorem ipsum
+ */
+//console.log('Linha 2')
+/* console.log('Linha 3')
+console.log('Linha 4')*/
+//Vai ser ignorado
